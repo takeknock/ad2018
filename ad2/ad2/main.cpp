bcbc0318cfc3d4b68764525b55c6a0892096cbd9
mmatthew <takeknock@gmail.com>
2018-04-10 00:55:39 +0900

    1H

---
+++ ad2/ad2/main.cpp
@@ -0,0 +1,10 @@
+#include "ad/dual_number.h"
+
+
+
+int main() {
+    ad::dual_number<3> spot = ad::dual_number<3>(0);
+    ad::dual_number<3> volatility = ad::dual_number<3>(1);
+
+    return 0;
+}
